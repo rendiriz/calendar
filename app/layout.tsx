70b7fd4646339c44b49d5ecf69eaf58c4fb0ef3a
Rendi Rizki <rendirizkir@gmail.com>
2024-02-10 17:43:44 +0700

    feat: partytown

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
+import { Partytown } from '@builder.io/partytown/react';
 import { cn } from '@/lib/utils';
 import { fontSerif, fontSans } from '@/lib/fonts';
 
@@ -26,6 +27,29 @@ export default function RootLayout({ children }: Props) {
         )}
       >
         {children}
+
+        <Partytown
+          debug={false}
+          forward={['dataLayer.push']}
+        />
+        <script
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.PUBLIC_GA_TRACKING_ID}`}
+          async
+          type="text/partytown"
+        />
+        <script
+          async
+          type="text/partytown"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+            
+              gtag('config', '${process.env.PUBLIC_GA_TRACKING_ID}');
+            `,
+          }}
+        />
       </body>
     </html>
   );
